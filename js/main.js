9b833de177de353ba8654c1b47a5a48d0917ad90
glau-isidoro <glaucia.isidoro@gmail.com>
2017-06-03 15:08:18 -0300

    cat clicker 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
-$("img").click(function(e) {
-    var contador = parseInt($("#count").text());
-    contador++
-    $("#count").text(contador);
+var Cat = function(name, img, i) {
+    this.name = name;
+    this.img = img;
+    this.i = i;
+    this.count = 0;
+}
+
+Cat.prototype.render = function() {
+    $(".cat-clicker").append("<li class='" + this.i + "'><img src='" + this.img + "' alt='gato' class='img-responsive'>"
+    + "<span class='name'>" + this.name + "</span> <span class='count'>" + this.count + "</span></li>")
+}
+
+Cat.prototype.counter = function() {
+  this.count++;
+  console.log(this.name + " - " + this.count)
+  $('.' + this.i + ' .count').text(this.count);
+}
+
+var cat1 = new Cat("Biscoito", "img/gato1.jpg", "cat1");
+var cat2 = new Cat("Bolacha", "img/gato2.jpg", "cat2");
+
+cat1.render();
+cat2.render();
+
+$("li").on("click", function (event) {
+    var catObj = eval($(this).attr("class"));
+    catObj.counter();
 });
